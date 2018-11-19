16638fea84125034332a25a795f8655eb2b080e9
Елена Портная <e.portnaya@largespace.ru>
2018-11-19 16:54:33 +0200

    forms php

--- app/js/common.js
+++ app/js/common.js
@@ -117,7 +117,22 @@ for (var i = 0; i < linkNav.length; i++) {
       }
   }, false);
 }
-
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
 
 });
